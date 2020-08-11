2c56b45145010958173d4cccb9597a5d7c41849b
Theo <theo.eschlimann@gmail.com>
2020-08-11 15:12:12 +0200

    Items quantity fixed

--- js/panier.js
+++ js/panier.js
@@ -32,7 +32,7 @@ function displayingItems(item) {
             <div class="item-name | col-lg-2 col-md-6 font-weight-bold d-flex justify-content-center align-items-center"> ${item.name} </div>
             <div class="item-option | col-lg-2 col-md-6 font-weight-bold d-flex justify-content-center align-items-center"> ${item.lenses}</div>
             <div class="col-lg-2 col-md-6 font-weight-bold d-flex justify-content-center align-items-center"> 
-                <div class="item-quantity"> 1 </div>
+                <div class="item-quantity" data-id="${item._id}"> 1 </div>
                 <div class="quantity-btn | d-flex flex-column pl-3">
                     <i class="quantity-btn-up |  fas fa-chevron-up" data-id="${item._id}"></i>
                     <i class="quantity-btn-down |  fas fa-chevron-down" data-id="${item._id}"></i>
@@ -40,7 +40,7 @@ function displayingItems(item) {
             </div>
             <div class="item-price | col-lg-2 col-md-6 font-weight-bold d-flex justify-content-center align-items-center" data-id="${item._id}">${item.price /100} €</div>
             <div class="col-lg-1 col-md-12 font-weight-bold d-flex justify-content-center align-items-center">
-                <a class="item-delete | btn btn-outline-danger" href="#" role="button">Supprimer</a>
+                <a class="item-delete | btn btn-outline-danger" href="#" role="button" data-id="${item._id}">Supprimer</a>
             </div>
         </div>
         <!-- Single item end -->
@@ -49,38 +49,80 @@ function displayingItems(item) {
     newDiv.innerHTML = display;
 }
 
+//Total Price
+function displayingTotalPrice() {
+
+    let itemPrice = document.querySelectorAll(".item-price");
+    let itemsTotalPrice = 0;
+
+    itemPrice.forEach(price => {
+        itemsTotalPrice += parseInt(price.textContent, 10);
+    });
+
+    totalPrice.innerHTML = itemsTotalPrice + " €";
+}
+
 //Item quantity
 function selectQuantity(item) {
-    let itemQuantity = document.querySelector(".item-quantity");
-    let quantityUp = document.querySelector(".quantity-btn-up");
-    let quantityDwn = document.querySelector(".quantity-btn-down");
-    let itemPrice = document.querySelector(".item-price");
+    let itemQuantity = document.querySelectorAll(".item-quantity");
+    let quantityUp = document.querySelectorAll(".quantity-btn-up");
+    let quantityDwn = document.querySelectorAll(".quantity-btn-down");
+    let itemPrice = document.querySelectorAll(".item-price");
 
-    quantityUp.addEventListener("click", () => {
-        let quantity = parseInt(itemQuantity.textContent, 10);
-        console.log(quantity);
-        quantity++;
-        itemQuantity.innerHTML = quantity;
-        itemPrice.innerHTML = quantity * item.price / 100 + " €";
+
+
+    quantityUp.forEach(btnUp => {
+        btnUp.addEventListener("click", (event) => {
+
+            if (event.target.getAttribute("data-id") === item._id) {
+                itemQuantity.forEach(quantity => {
+
+                    if (quantity.getAttribute("data-id") === item._id) {
+                        let qty = parseInt(quantity.textContent, 10);
+                        qty++;
+
+                        itemPrice.forEach(price => {
+                            if (price.getAttribute("data-id") === item._id) {
+                                quantity.innerHTML = qty;
+                                price.innerHTML = qty * item.price / 100 + " €";
+                            };
+                        });
+                    };
+
+                });
+            };
+            displayingTotalPrice()
+        });
     });
 
-    quantityDwn.addEventListener("click", () => {
-        let quantity = parseInt(itemQuantity.textContent, 10);
-        console.log(quantity);
-        quantity--;
-        if (quantity <= 0) {
-            quantity = 1;
-        }
-        itemQuantity.innerHTML = quantity;
-        itemPrice.innerHTML = quantity * item.price / 100 + " €";
-    });
-}
+    quantityDwn.forEach(btnDwn => {
+        btnDwn.addEventListener("click", (event) => {
 
-//Total Price
-function displayingTotalPrice(item) {
-    itemsTotalPrice += item.price;
-    totalPrice.innerHTML = itemsTotalPrice / 100 + " €";
-}
+            if (event.target.getAttribute("data-id") === item._id) {
+                itemQuantity.forEach(quantity => {
+
+                    if (quantity.getAttribute("data-id") === item._id) {
+                        let qty = parseInt(quantity.textContent, 10);
+                        qty--;
+
+                        if (qty <= 0) {
+                            qty = 1;
+                        };
+
+                        itemPrice.forEach(price => {
+                            if (price.getAttribute("data-id") == item._id) {
+                                quantity.innerHTML = qty;
+                                price.innerHTML = qty * item.price / 100 + " €";
+                            };
+                        });
+
+                    };
+                });
+            };
+            displayingTotalPrice()
+        });
+    });
+};
 
 //MAIN FUNCTION
 document.addEventListener("DOMContentLoaded", () => {
@@ -92,9 +134,7 @@ document.addEventListener("DOMContentLoaded", () => {
             item.lenses = lensesOption;
             displayingItems(item);
             selectQuantity(item);
-            displayingTotalPrice(item);
-
-            console.log(item);
+            displayingTotalPrice()
         });
     });
 });
